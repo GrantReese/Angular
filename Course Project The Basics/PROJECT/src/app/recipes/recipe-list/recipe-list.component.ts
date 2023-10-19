abaec434c906d10dc81d00557229e81fd95dcc94
Grant Reese <119977613+GrantReese@users.noreply.github.com>
2023-10-18 19:03:34 -0500

    Finished Course Project the Basics

--- Course Project The Basics/PROJECT/src/app/recipes/recipe-list/recipe-list.component.ts
+++ Course Project The Basics/PROJECT/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -9,6 +9,7 @@ import { Recipe } from '../recipe.model';
 })
 export class RecipeListComponent {
 recipes: Recipe[] = [
-  new Recipe('A Test Recipe', 'A recipe to test', 'https://hips.hearstapps.com/hmg-prod/images/crepes-index-64347419e3c7a.jpg?crop=0.888888888888889xw:1xh;center,top&resize=1200:*' )
+  new Recipe('A Test Recipe', 'A recipe to test', 'https://hips.hearstapps.com/hmg-prod/images/crepes-index-64347419e3c7a.jpg?crop=0.888888888888889xw:1xh;center,top&resize=1200:*' ),
+  new Recipe('A Test Recipe', 'A recipe to test', 'https://hips.hearstapps.com/hmg-prod/images/crepes-index-64347419e3c7a.jpg?crop=0.888888888888889xw:1xh;center,top&resize=1200:*' ),
 ];
 }
