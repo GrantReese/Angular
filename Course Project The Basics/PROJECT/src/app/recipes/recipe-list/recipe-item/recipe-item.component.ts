b25c543e8fd3a7c2112ad414e8ec6a79f9b27076
Grant Reese <119977613+GrantReese@users.noreply.github.com>
2023-10-31 19:45:24 -0500

    Finished course project and data binding
    Lost several features, will need to get help during office hours

--- Course Project The Basics/PROJECT/src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
+++ Course Project The Basics/PROJECT/src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
@@ -1,5 +1,5 @@
-import { Component, Input, OnInit } from '@angular/core';
-import { Recipe } from '../../recipe.model';
+import { Component, EventEmitter, Input, OnInit } from '@angular/core';
+import { Recipe } from '../../../shared/recipe.model';
 @Component({
   selector: 'app-recipe-item',
   templateUrl: './recipe-item.component.html',
@@ -7,11 +7,13 @@ import { Recipe } from '../../recipe.model';
 })
 export class RecipeItemComponent implements OnInit {
  @Input() recipe: Recipe;
-
+recipeSelected = new EventEmitter<void>();
   constructor() { }
 
   ngOnInit() {
 
   }
-
+onSelected(){
+this.recipeSelected.emit();
+}
 }
--- Course Project The Basics/PROJECT/src/app/recipes/recipe-list/recipe-list.component.ts
+++ Course Project The Basics/PROJECT/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,15 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 
-import { Recipe } from '../recipe.model';
+import { Recipe } from '../../shared/recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
   styleUrls: ['./recipe-list.component.css']
 })
-export class RecipeListComponent {
+export class RecipeListComponent implements OnInit {
+ @Output() recipeWasSelected = new EventEmitter<Recipe>();
 recipes: Recipe[] = [
   new Recipe('A Test Recipe', 'A recipe to test', 'https://hips.hearstapps.com/hmg-prod/images/crepes-index-64347419e3c7a.jpg?crop=0.888888888888889xw:1xh;center,top&resize=1200:*' ),
-  new Recipe('A Test Recipe', 'A recipe to test', 'https://hips.hearstapps.com/hmg-prod/images/crepes-index-64347419e3c7a.jpg?crop=0.888888888888889xw:1xh;center,top&resize=1200:*' ),
+  new Recipe('Another Test Recipe', 'A recipe to test', 'https://hips.hearstapps.com/hmg-prod/images/crepes-index-64347419e3c7a.jpg?crop=0.888888888888889xw:1xh;center,top&resize=1200:*' ),
 ];
+constructor() {}
+
+ngOnInit() {}
+
+onRecipeSelected(recipe: Recipe) {
+  this.recipeWasSelected.emit(recipe);
+}
 }
